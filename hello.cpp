f8fc1b415505ea6460bb068b90f5298bb45859a2
iamAzhar <mohdazhar6117@gmail.com>
2021-08-30 20:36:19 +0800

    first commit

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+    string name;
+   cout << "hello world" << endl;
+   cout << "Enter your name: " << endl;
+   cin >> name;
+   for (int i=0 ; i<10; i++){
+       cout << "your name is "<<name<<endl;
+   };
+
+   return 0;
+}
